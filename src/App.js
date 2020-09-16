712ca25e4231f08aa9a1e0b5896bea137d21404f
John Leavell <johnleavell@gmail.com>
2020-09-16 16:51:58 -0500

    added login component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+
 import './App.css';
-// import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
 import NavDrawer from './components/NavDrawer'
 
 
 class App extends Component {
   render(){
     return (
-      
-      <div className="App">
-        <NavDrawer />
+      <>
+      <NavDrawer />
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                </ul>
+              </div>
+            </div>
+          </nav>
 
-      </div>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Switch>
+                <Route exact path='/' component={Login} />
+                <Route path="/log-in" component={Login} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+      </>
     );
   }
 }
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,41 +1,12 @@
 import React, { Component } from 'react';
-import {Radar} from 'react-chartjs-2';
 
-const xlabels = [];   
-const data = {
-  labels: xlabels,
-  datasets: [
-    {
-      label: 'My First dataset',
-      backgroundColor: 'rgba(179,181,198,0.2)',
-      borderColor: 'rgba(179,181,198,1)',
-      pointBackgroundColor: 'rgba(179,181,198,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(179,181,198,1)',
-      data: [65, 59, 90, 81, 56, 55, 40]
-    },
-    {
-      label: 'My Second dataset',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      pointBackgroundColor: 'rgba(255,99,132,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(255,99,132,1)',
-      data: [28, 48, 40, 19, 96, 27, 100]
-    }
-  ]
-};
 export default class Dashboard extends Component {
   constructor(props){
     super(props);
     this.state = {
       reports: [],
-      displayName: 'DoughnutExample'
     }
   }
-
   componentDidMount(){
     this.fetchResults();
   };
@@ -50,8 +21,7 @@ export default class Dashboard extends Component {
 
     return (
       <div>
-        <Radar data={data}
-        />
+
       </div>
     )
   }
